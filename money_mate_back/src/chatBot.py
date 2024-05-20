23fa3129964a915a5c61ef26c20c465a8f1cba7a
Juan Pablo Giraldo <juan@netprogroup.com>
2024-05-20 16:50:49 -0500

    Corrijo funcionamiento de modelo

--- money_mate_back/src/chatBot.py
+++ money_mate_back/src/chatBot.py
@@ -1,5 +1,7 @@
 import random
-import pickle
+import joblib
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 class ChatBot:
     def __init__(self):
@@ -11,14 +13,8 @@ class ChatBot:
         self.imprevistos_mes = 0
         self.gastos_hormiga = 0
         self.step = 0
-
-        try:
-            # Carga el modelo utilizando pickle
-            with open('./util/modelito1.pkl', 'rb') as f:
-                self.model = pickle.load(f)
-            print("Modelo cargado exitosamente:", self.model)
-        except Exception as e:
-            print(f"Error al cargar el modelo: {e}")
+        self.model = joblib.load('./util/modelito1.pkl')
+        self.scaler = joblib.load('./util/scaler.pkl')
 
     def process_message_text(self, message):
         if isinstance(message, list):
@@ -28,24 +24,26 @@ class ChatBot:
         self.highest_prob.clear()  # Clear the dictionary for each new message
 
         def response(bot_response, list_of_words, single_response=False, required_words=[]):
+            print(len(list_of_words))
             self.highest_prob[bot_response] = self.message_probability(message, list_of_words, single_response, required_words)
 
-        print(message)
+        if message[0] == 'Hola': 
+            self.step = 0
         # Pregunta inicial
         if self.step == 0:
             response('Hola, ¿cómo te llamas?', ['hola', 'saludos', 'buenas', 'ey'], single_response=True)
         elif self.step == 1:
             response(f'{self.nombre}, ¿cuál es tu salario mensual?', [], single_response=True)
         elif self.step == 2:
-            response(f'{self.nombre}, ¿cuál es tu ingreso fijo mensual?', [], single_response=True)
+            response(f'{self.nombre}, ¿Es este salario un monto fijo? 1: Si, 0: No', [], single_response=True)
         elif self.step == 3:
-            response(f'{self.nombre}, ¿cuánto son tus gastos mensuales?', [], single_response=True)
+            response(f'{self.nombre}, ¿Cuánto suman tus gastos mensuales?', ['1', '0'], single_response=True)
         elif self.step == 4:
             response(f'{self.nombre}, ¿cuánto estimas en imprevistos al mes?', [], single_response=True)
         elif self.step == 5:
-            response(f'{self.nombre}, ¿cuánto gastas en gastos hormiga al mes?', [], single_response=True)
+            response(f'{self.nombre}, ¿cómo categorizas tus gastos hormiga? 1: Pocos, 2: Moderados, 3: Bastante, 4: Exagerado ?', [], single_response=True)
         elif self.step == 6:
-            response(f'Análisis finalizado, {self.nombre}. Predicción: ', [], single_response=True)
+            response(f'Análisis finalizado, {self.nombre}. Predicción: ', ['1', '2', '3', '4'], single_response=True)
         
         best_match = max(self.highest_prob, key=self.highest_prob.get)
 
@@ -62,36 +60,34 @@ class ChatBot:
         elif self.step == 2:
             try:
                 self.salario = int(message[0])
+                self.step = 3
+                return f'{self.nombre}, ¿Es este salario un monto fijo? <br><br> Elige una de las siguientes opciones: <br> 1. Si <br> 0. No'
             except ValueError:
                 return f'{self.nombre}, por favor ingresa un número válido para el salario.'
-            self.step = 3
-            return f'{self.nombre}, ¿cuál es tu ingreso fijo mensual?'
         elif self.step == 3:
-            try:
-                self.ingreso_fijo = int(message[0])
-            except ValueError:
-                return f'{self.nombre}, por favor ingresa un número válido para el ingreso fijo.'
+            if message[0] not in ['1', '0']:
+                return f'{self.nombre}, por favor responde con 1 (Sí) o 0 (No) para la pregunta anterior.'
+            self.ingreso_fijo = int(message[0])
             self.step = 4
-            return f'{self.nombre}, ¿cuánto son tus gastos mensuales?'
+            return f'{self.nombre}, ¿Cuánto suman tus gastos mensuales?'
         elif self.step == 4:
             try:
                 self.gastos_mensuales = int(message[0])
+                self.step = 5
+                return f'{self.nombre}, ¿cuánto estimas en imprevistos al mes?'
             except ValueError:
                 return f'{self.nombre}, por favor ingresa un número válido para los gastos mensuales.'
-            self.step = 5
-            return f'{self.nombre}, ¿cuánto estimas en imprevistos al mes?'
         elif self.step == 5:
             try:
                 self.imprevistos_mes = int(message[0])
+                self.step = 6
+                return f'{self.nombre}, ¿cómo clasificarías tus gastos hormiga? <br><br> Elige una de las siguientes opciones: <br> 1. Poco <br> 2. Moderado <br> 3. Normal <br> 4. En exceso'
             except ValueError:
                 return f'{self.nombre}, por favor ingresa un número válido para los imprevistos.'
-            self.step = 6
-            return f'{self.nombre}, ¿cuánto gastas en gastos hormiga al mes?'
         elif self.step == 6:
-            try:
-                self.gastos_hormiga = int(message[0])
-            except ValueError:
-                return f'{self.nombre}, por favor ingresa un número válido para los gastos hormiga.'
+            if message[0] not in ['1', '2', '3', '4']:
+                return f'{self.nombre}, por favor responde con 1, 2, 3, o 4 para los gastos hormiga.'
+            self.gastos_hormiga = int(message[0])
             return self.run_prediction()
 
         return best_match
@@ -129,13 +125,18 @@ class ChatBot:
 
         input_data = [
             self.salario,
-            self.ingreso_fijo,
             self.gastos_mensuales,
             self.imprevistos_mes,
-            self.gastos_hormiga
         ]
-        prediction = self.model.predict([input_data])[0]
-        return f'Análisis finalizado, {self.nombre}. Predicción: {prediction}'
+
+        try:
+            self.step = 0
+            input_data_scaled = self.scaler.transform([input_data])
+            prediction = self.model.predict(input_data_scaled)[0]
+            message = 'Probablemente no habrá endeudamiento' if prediction == 0 else 'Probablemente si habrá endeudamiento'
+            return f'Análisis finalizado, {self.nombre}. Predicción: {message}'
+        except Exception as e:
+            return f'Error al realizar la predicción: {e}'
 
 # Código de prueba
 if __name__ == '__main__':
